fcb3f9052ccb43f25a3186731b52c1e82d8f9614
samer <samerbenmim@insat.u-carthage.tn>
2022-12-13 10:05:05 +0100

    add user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,8 +3,10 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user-dto';
 @Controller('auth')
 export class UsersController {
+
+    constructor(private  UsersService: UsersService) { }
     @Post('/signup')
     createUser(@Body() body: CreateUserDto){
-       // return this.usersService.createUser();
+        return this.UsersService.create(body.email,body.password);
     }
 }
