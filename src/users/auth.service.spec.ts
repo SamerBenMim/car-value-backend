f9a3b5beabfb8432770f15292c1c6656de1c4287
samer <samerbenmim@insat.u-carthage.tn>
2022-12-16 20:59:10 +0100

    testing authentification

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -4,13 +4,16 @@ import { UsersService } from "../users/users.service";
 import { User } from "./user.entity";
 
 describe('AuthService', () => {
-    let fakeUserService: Partial<UsersService> 
+    let fakeUserService: Partial<UsersService>
     let service: AuthService;
     beforeEach(async () => {
-
-         fakeUserService= {
-            find: () => Promise.resolve([]),
-            create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User)
+        const users : User[] = [];
+        fakeUserService = {
+            find: (email : string) => Promise.resolve(users.filter(user => user.email === email)),
+            create: (email: string, password: string) =>{
+                users.push({ id: Math.floor(Math.random()*9999), email, password } as User);
+                return Promise.resolve(users[users.length-1])    
+            } 
         }
 
         const module = await Test.createTestingModule({
@@ -36,16 +39,43 @@ describe('AuthService', () => {
         const [salt, hash] = user.password.split('.');
         expect(salt).toBeDefined();
         expect(hash).toBeDefined();
-    });    
+    });
 
-    it('throws an error if user signs up with email that is in use', async () => {
-       fakeUserService.find = () => Promise.resolve([{id:1,email:'a',password:'1'} as User]);
+
+    it('throws if signin is called with an unused email', async () => {
         try {
             await service.signup('a', '1');
             //expect(true).toBe(false);
-        }catch (err) {
+        } catch (err) {
+            expect(err.message).toEqual('user not found');
+        }
+    });
+    it('throws an error if user signs up with email that is in use', async () => {
+        //fakeUserService.find = () => Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+        await service.signup('a', '2');
+
+        try {
+            await service.signup('a', '1');
+            // expect(true).toBe(false);
+        } catch (err) {
             expect(err.message).toEqual('email in use');
         }
     });
+    it('throws an error if invalid password is provided', async () => {
+        // fakeUserService.find = () => Promise.resolve([{ id: 1, email: 'test', password: 'wrong' } as User]);
+        await service.signup('test', 'password');
+        try {
+            await service.signin('test', 'wrong');
+
+        } catch (err) {
+            expect(err.message).toEqual('bad password');
+        }
+    });
+    it('returns a user if correct password is provided', async () => {
+        await service.signup('test@test.test', 'mypassword');
+        const user = await service.signin('test@test.test', 'mypassword');
+        expect(user).toBeDefined();
+    });
+
 
 });
