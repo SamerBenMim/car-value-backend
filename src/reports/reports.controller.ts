eed89fcdf908ab0c2f7ce1ecfc2a3283b7895a0c
samer <samerbenmim@insat.u-carthage.tn>
2022-12-17 22:22:42 +0100

    add guard

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './create-user.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guards/auth.gard';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+    constructor(private readonly reportsService: ReportsService) { }
+    @Post()
+    @UseGuards(AuthGuard)
+    createReport(@Body() body: CreateReportDto) {
+        return this.reportsService.createReport(body);
+    }
+
+
+}
