839e35f062cff9f2f932cc08a70cdab46b11ef64
samer <samerbenmim@insat.u-carthage.tn>
2022-12-17 22:55:48 +0100

    report servic

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './reports.entity';
+import { CreateReportDto } from './create-user.dto';
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+     constructor(
+        @InjectRepository(Report)
+        private readonly reportsRepository: Repository<Report>,
+    ) { }
+    async createReport(report: CreateReportDto) {
+        const newReport = this.reportsRepository.create(report);
+        await this.reportsRepository.save(newReport);
+        return newReport;
+    }
+
+}
